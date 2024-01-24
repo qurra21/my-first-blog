c9d1a9baa2aedb6473fca527c558172f97c04c06
qurra21 <sakura.saroun@student.cadt.edu.kh>
2024-01-24 16:01:40 +0700

    Modified template

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,14 @@
 from django.shortcuts import render
+from django.utils import timezone
+from .models import Post # . means current directory
+# voth model.py and view.py are in the sam dir
+# so can use .
+
+def post_list(request) : 
+    posts = Post.objects.filter(published_date_lte= timezone.now()).order_by('published_date')
+    return render(request, 'blog/post_list.html', {'posts': posts})
+
+
 
 
 
