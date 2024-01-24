083e0f26067198fac5873a0e8471db0afb8912b7
qurra21 <sakura.saroun@student.cadt.edu.kh>
2024-01-24 15:15:38 +0700

    change html for the site

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
+
+
+# request the info form the model you create before
+# then pass it to template
 # Create your views here.
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
